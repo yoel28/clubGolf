a855d6903825079aad141d8837e8bab7db56b804
yo3l28 <yo3l.m18@gmail.com>
2016-12-04 19:13:02 -0400

    lista de directivas de la aplicacion

--- appRoot/app.directives.ts
+++ appRoot/app.directives.ts
@@ -1,4 +1,12 @@
 import {ColorPicker} from "./com.zippyttech.utils/directive/colorPicker";
+import {XEditable} from "./com.zippyttech.utils/directive/xEditable";
+import {XCropit} from "./com.zippyttech.utils/directive/xCropit";
+import {XFile} from "./com.zippyttech.utils/directive/xFile";
 
-export const directivesDefault = [ColorPicker];
+export const directivesDefault = [
+    ColorPicker,
+    XEditable,
+    XCropit,
+    XFile
+];
 export const directivesApp = [];
