3c4a9662d9bf13a2520c346bc2966c2c742c72ab
yo3l28 <yo3l.m18@gmail.com>
2016-12-04 19:13:56 -0400

    validacion por variables de cntrol

--- appRoot/com.zippyttech.utils/globalService.ts
+++ appRoot/com.zippyttech.utils/globalService.ts
@@ -1,65 +1,68 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from "@angular/http";
 import {RestController} from "../com.zippyttech.rest/restController";
 import {contentHeaders} from "../com.zippyttech.rest/headers";
+import {FormControl, Validators} from "@angular/forms";
 
 @Injectable()
 export class globalService extends RestController{
-    user:any=[];
+    user:any={};
     params:any={};
     help:any={};
     rules:any={};
     permissions:any=[];
-    init=false;
 
-    status={
-        'token':{'status':false,'title':'Validando usuario'},
-        'user':{'status':false,'title':'Consultando datos del usuario'},
-        'permissions':{'status':false,'title':'Consultando  permisos'},
-        'params':{'status':false,'title':'Consultando  parametros'},
-        'help':{'status':false,'title':'Consultando  ayudas'},
-        'rules':{'status':false,'title':'Consultando  reglas'},
-    };
+    public dataSesion = new FormControl(
+        null,
+        Validators.compose([
+            (c:FormControl)=> {
+                if(c.value){
+                    if( c.value.token.status &&
+                        c.value.user.status  &&
+                        c.value.permissions.status &&
+                        c.value.params.status &&
+                        c.value.help.status &&
+                        c.value.rules.status
+                    )
+                    {return null;}
+                }
+                return {object: {valid: false}};
+            }
+        ])
+    );
+    public saveUrl:string;
 
     objectInstance:any={};//lista de instancias creadas
     
     constructor(public http:Http) {
         super(http);
-
         if (typeof(Storage) !== "undefined") {
             console.log("habemus localstorage")
         } else {
             console.log("no habemus localstorage")
         }
-
-        if(localStorage.getItem('bearer')){
-            this.initSession();
-        }
     }
     initSession():void{
-        this.initFinish(true);
+        this.dataSesionInit();
         this.loadValidToken();
         this.loadMyPermissions();
         this.loadParams();
         this.loadTooltips();
         this.loadRules();
     }
-    initFinish(reverse=false):void{
-        if(reverse)
-        {
-            this.status.token.status=false;
-            this.status.user.status=false;
-            this.status.permissions.status=false;
-            this.status.params.status=false;
-            this.status.help.status=false;
-            this.status.rules.status=false;
-        }
-        if(this.status.token.status && this.status.user.status  && this.status.permissions.status && this.status.params.status && this.status.help.status && this.status.rules.status)
-            this.init=true;
+    dataSesionInit():void{
+        this.dataSesion.setValue({
+            'token':        {'status':false,'title':'Validando usuario'},
+            'user':         {'status':false,'title':'Consultando datos del usuario'},
+            'permissions':  {'status':false,'title':'Consultando  permisos'},
+            'params':       {'status':false,'title':'Consultando  parametros'},
+            'help':         {'status':false,'title':'Consultando  ayudas'},
+            'rules':        {'status':false,'title':'Consultando  reglas'}
+        });
     }
     error = (err:any):void => {
         if(localStorage.getItem('bearer')){
-            this.initFinish(true);
+            this.dataSesionInit();
             localStorage.removeItem('bearer');
             contentHeaders.delete('Authorization');
             window.location.reload();
@@ -69,8 +72,8 @@ export class globalService extends RestController{
         let that=this;
         let successCallback=(response:any) => {
             Object.assign(that.user, response.json());
-            that.status.token.status=true;
-            that.initFinish();
+            that.dataSesion.value.token.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
             that.loadUser();
         };
         this.httputils.doGet('/validate',successCallback,this.error);
@@ -79,8 +82,9 @@ export class globalService extends RestController{
         let that = this;
         let successCallback= (response:any) => {
             Object.assign(that.user,that.user,response.json().list[0]);
-            that.status.user.status=true;
-            that.initFinish();
+            that.dataSesion.value.user.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
+
         };
         let where = encodeURI('[["op":"eq","field":"username","value":"'+this.user.username+'"]]');
         this.httputils.doGet('/users?where='+where, successCallback,this.error);
@@ -89,8 +93,8 @@ export class globalService extends RestController{
         let that = this;
         let successCallback= (response:any) => {
             Object.assign(that.permissions,response.json());
-            that.status.permissions.status=true;
-            that.initFinish();
+            that.dataSesion.value.permissions.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
         };
         return this.httputils.doGet('/current/permissions/',successCallback,this.error);
     }
@@ -98,8 +102,8 @@ export class globalService extends RestController{
         let that = this;
         let successCallback= (response:any) => {
             Object.assign(that.params,response.json().list);
-            that.status.params.status=true;
-            that.initFinish();
+            that.dataSesion.value.params.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
         };
         this.httputils.doGet('/params?max=1000',successCallback,this.error);
     }
@@ -107,8 +111,8 @@ export class globalService extends RestController{
         let that = this;
         let successCallback= (response:any) => {
             Object.assign(that.rules,response.json().list);
-            that.status.rules.status=true;
-            that.initFinish();
+            that.dataSesion.value.rules.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
         };
         this.httputils.doGet('/rules?max=1000',successCallback,this.error);
     }
@@ -116,8 +120,8 @@ export class globalService extends RestController{
         let that = this;
         let successCallback= (response:any) => {
             Object.assign(that.help,response.json().list);
-            that.status.help.status=true;
-            that.initFinish();
+            that.dataSesion.value.help.status=true;
+            that.dataSesion.setValue(that.dataSesion.value);
         };
         this.httputils.doGet('/infos?max=1000',successCallback,this.error);
     }
@@ -132,39 +136,39 @@ export class globalService extends RestController{
     getParams(key:string):string{
         let that = this;
         let valor="";
-        Object.keys(this.params).forEach(index=>{
+        Object.keys(this.params || {}).forEach(index=>{
             if(that.params[index].key==key){
                 valor=that.params[index].value;
                 return;
             }
-        })
+        });
         return valor;
     }
 
     getRule(key:string):string{
         let that = this;
         let valor="";
-        Object.keys(this.rules).forEach(index=>{
+        Object.keys(this.rules || {}).forEach(index=>{
             if(that.rules[index].key==key){
                 valor=that.rules[index].value;
                 return;
             }
-        })
+        });
         return valor;
     }
     getTooltip(code:string):any{
         let that = this;
         let valor={};
-        Object.keys(this.help).forEach(index=>{
+        Object.keys(this.help || {}).forEach(index=>{
             if(that.help[index].code==code){
                 valor=that.help[index];
                 return;
             }
-        })
+        });
         return valor;
     }
     getKeys(data:any):any{
-        return Object.keys(data);
+        return Object.keys(data || {});
     }
     
 }
