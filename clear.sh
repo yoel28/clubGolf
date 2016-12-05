e74544fdd13d271cc259c5f2293beed744ddb314
yoel <yo3l.m18@gmail.com>
2016-12-05 10:45:29 -0400

    map

--- clear.sh
+++ clear.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 find ./appRoot -type f -name '*.js' -exec rm -f {} \;
-find ./appRoot -type f -name '*.js.msp' -exec rm -f {} \;
+find ./appRoot -type f -name '*.js.map' -exec rm -f {} \;
 rm -f ./system-config.js;
 rm -f ./system-config.js.map;
-rm -r dist/
+rm -rf dist/
