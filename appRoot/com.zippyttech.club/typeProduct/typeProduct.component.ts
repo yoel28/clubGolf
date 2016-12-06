7b00d176c06efa10a9c535b67059cfaec6e72a85
yoel <yo3l.m18@gmail.com>
2016-12-06 16:36:32 -0400

    controlador y modelo de type product

---
+++ appRoot/com.zippyttech.club/typeProduct/typeProduct.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {globalService} from "../../com.zippyttech.utils/globalService";
+import {TypeProductModel} from "./typeProduct.model";
+
+declare var SystemJS:any;
+@Component({
+    selector: 'type-product',
+    templateUrl:SystemJS.map.app+'/com.zippyttech.ui/view/base/base.html',
+    styleUrls: [SystemJS.map.app+'/com.zippyttech.ui/view/base/style.css'],
+})
+export class TypeProductComponent implements OnInit,AfterViewInit{
+
+    public instance:any={};
+    public paramsTable:any={};
+    public model:any;
+    public viewOptions:any={};
+
+    constructor(public myglobal:globalService) {}
+
+    ngOnInit(){
+        this.initModel();
+        this.initViewOptions();
+        this.loadParamsTable();
+    }
+
+    ngAfterViewInit():any {
+        this.instance = {
+            'model':this.model,
+            'viewOptions':this.viewOptions,
+            'paramsTable':this.paramsTable
+        };
+    }
+
+    initModel() {
+        this.model= new TypeProductModel(this.myglobal);
+    }
+
+    initViewOptions() {
+        this.viewOptions["title"] = 'Tipo de producto';
+    }
+
+    loadParamsTable(){
+        this.paramsTable.actions={};
+        this.paramsTable.actions.delete = {
+            'message': '¿ Esta seguro de eliminar el tipo: ',
+            'keyAction':'title'
+        };
+    }
+}
