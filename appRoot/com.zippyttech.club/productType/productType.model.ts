4dafcfc893d9eae39ddc031b4cbbb61e73b13bf8
yoel <yo3l.m18@gmail.com>
2016-12-06 17:07:20 -0400

    es product type

---
+++ appRoot/com.zippyttech.club/productType/productType.model.ts
@@ -0,0 +1,132 @@
+import {globalService} from "../../com.zippyttech.utils/globalService";
+import {ModelBase} from "../../com.zippyttech.common/modelBase";
+import {StaticValues} from "../../com.zippyttech.utils/catalog/staticValues";
+
+export class ProductTypeModel extends ModelBase{
+    public rules={};
+    public pathElements=StaticValues.pathElements;
+
+    constructor(public myglobal:globalService){
+        super('PRTYPE','/type/products/',myglobal);
+        this.initModel();
+    }
+    modelExternal() {}
+    initRules(){
+        this.rules['autoReceive']={
+            'type':'boolean',
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'autoReceive',
+            'icon': 'fa fa-key',
+            'title':'Auto recibido',
+            'placeholder':'Auto recibido',
+            'source': [
+                {'value':true, 'text': 'Activado', 'class': 'btn btn-sm btn-green'},
+                {'value':false,'text': 'Desactivado', 'class': 'btn btn-sm btn-red'},
+            ],
+        };
+
+        this.rules['icon']= {
+            'type': 'image',
+            'required':false,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key': 'icon',
+            'icon': 'fa fa-key',
+            'default':this.pathElements.warning,
+            'title': 'Imagen',
+            'placeholder': 'Ingrese una imagen',
+        };
+
+        this.rules['title']={
+            'type':'text',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'title',
+            'icon': 'fa fa-list',
+            'title':'Nombre',
+            'placeholder':'Nombre',
+        };
+
+        this.rules['type']={
+            'type':'select',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'type',
+            'title':'Tipo',
+            'placeholder':'Tipo',
+            'source':[
+                {'value':'unidad','text': 'Unidad', 'class': 'btn btn-sm btn-green'},
+                {'value':'unidad','text': 'Tiempo', 'class': 'btn btn-sm btn-red'},
+            ]
+        };
+        this.rules['minPrice']={
+            'type':'number',
+            'required':true,
+            'double':true,
+            'step':'0.1',
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'minPrice',
+            'icon': 'fa fa-list',
+            'title':'Precio mínimo',
+            'placeholder':'Precio mínimo',
+        };
+        this.rules['minMinutes']={
+            'type':'number',
+            'required':true,
+            'double':true,
+            'step':'0.1',
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'minMinutes',
+            'icon': 'fa fa-list',
+            'title':'Minutos mínimos',
+            'placeholder':'Minutos mínimos',
+        };
+        this.rules['price']={
+            'type':'number',
+            'required':true,
+            'double':true,
+            'step':'0.01',
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key':'price',
+            'icon': 'fa fa-list',
+            'title':'Precio',
+            'placeholder':'Precio',
+        };
+
+        this.rules = Object.assign({},this.rules,this.getRulesDefault());
+    }
+    initPermissions() {}
+    initParamsSearch() {
+        this.paramsSearch.title="Buscar tipo de producto";
+        this.paramsSearch.placeholder="Ingrese codigo del tipo de producto";
+    }
+    initParamsSave() {
+        this.paramsSave.title="Agregar tipo de producto"
+    }
+    initRuleObject() {
+        this.ruleObject.title="Tipo de producto";
+        this.ruleObject.placeholder="Ingrese codigo del tipo de producto";
+        this.ruleObject.key="productType";
+        this.ruleObject.keyDisplay = "productTypeTitle";
+        this.ruleObject.code = "productTypeId";
+    }
+    initRulesSave() {
+        this.rulesSave = Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+        delete this.rulesSave.autoReceive;
+        delete this.rulesSave.icon;
+    }
+}
