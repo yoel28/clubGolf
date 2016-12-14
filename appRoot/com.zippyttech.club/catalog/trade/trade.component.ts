32287413ee3cc1f7a39ded46ba75563354dfd89c
yoel <yo3l.m18@gmail.com>
2016-12-14 13:48:53 -0400

    push en array de where

--- appRoot/com.zippyttech.club/catalog/trade/trade.component.ts
+++ appRoot/com.zippyttech.club/catalog/trade/trade.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {globalService} from "../../../com.zippyttech.utils/globalService";
 import {TradeModel} from "./trade.model";
 import {BaseViewInstance} from "../../../com.zippyttech.ui/view/base/baseView.instance";
-import {UserModel} from "../../../com.zippyttech.access/user/user.model";
 import {ActivatedRoute} from "@angular/router";
 
 declare var SystemJS:any;
@@ -13,8 +12,7 @@ declare var SystemJS:any;
 })
 export class TradeComponent extends BaseViewInstance{
 
-    public sponsor:any;
-    public guest:any;
+
     public userId:string;
 
     constructor(public myglobal:globalService,private routeActive: ActivatedRoute) {
@@ -25,30 +23,18 @@ export class TradeComponent extends BaseViewInstance{
         this.userId=this.routeActive.snapshot.params['userId'];
 
         if(this.userId){
-            this.rest.where={'or':[]};
-            this.rest.where ['or'].push({'op':'eq', 'value':parseFloat(this.userId), 'field':'sponsor.id'});
-            this.rest.where ['or'].push({'op':'eq', 'value':parseFloat(this.userId), 'field':'guest.id'});
+            this.rest.where.push
+            ({
+                'or':[
+                        {'op':'eq', 'value':parseFloat(this.userId), 'field':'s.id'},
+                        {'op':'eq', 'value':parseFloat(this.userId), 'field':'g.id'}
+                    ]
+            });
         }
     }
 
     initModel() {
         this.model= new TradeModel(this.myglobal);
-        this.sponsor = new UserModel(this.myglobal);
-        this.guest = new UserModel(this.myglobal);
-
-        this.model.rules['sponsor'] = Object.assign({},this.sponsor.ruleObject);
-        this.model.rules['sponsor'].key='sponsor';
-        this.model.rules['sponsor'].title='Patrocinador';
-        this.model.rules['sponsor'].keyDisplay='sponsorName';
-        this.model.rules['sponsor'].placeholder='Patrocinador';
-        this.model.rules['sponsor'].paramsSearch.field='sponsor.id';
-
-        this.model.rules['guest'] = Object.assign({},this.guest.ruleObject);
-        this.model.rules['guest'].key='guest';
-        this.model.rules['guest'].title='Invitado';
-        this.model.rules['guest'].keyDisplay='guestName';
-        this.model.rules['guest'].placeholder='Invitado';
-        this.model.rules['guest'].paramsSearch.field='guest.id';
 
     }
 
