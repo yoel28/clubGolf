08d39958e8c5e3e191506eaee139e224886a4d73
yoel <yo3l.m18@gmail.com>
2016-12-15 16:38:25 -0400

    casi falta un estado

--- appRoot/com.zippyttech.utils/websocket.ts
+++ appRoot/com.zippyttech.utils/websocket.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {globalService} from "./globalService";
 import {FormControl} from "@angular/forms";
 
@@ -7,11 +7,17 @@ declare var SockJS:any;
 declare var Stomp:any;
 
 @Injectable()
-export class WebSocket {
+export class WebSocket implements OnDestroy{
+    /*
+    * this.ws.client.ws.readyState
+    * 0= Nada
+    * 1= Conectado
+    * 3= Desconectado
+    * */
 
     public client:any;
-    public forceClose=false;
     public status:FormControl;
+    public threat:any;
 
     constructor(public myglobal:globalService){
         this.status = new FormControl(false);
@@ -26,25 +32,32 @@ export class WebSocket {
         this.client.send("/app/message", {priority: 9}, value);
     }
     onConnect(channel){
-        let that = this;
-        let ws = new SockJS(localStorage.getItem('url') + "/stomp");
-        this.client = Stomp.over(ws);
-        that.client.connect({},
-            function () {
-                that.status.setValue(true);
-                that.client.subscribe(channel, function (message) {
-                    that.myglobal.channelWebsocket[channel].setValue(JSON.parse(message.body));
-                });
-            },
-            function(error) {
-                that.status.setValue(false);
-                setTimeout(
-                    function(){
-                        if(!that.forceClose)
+        try {
+            let that = this;
+            let ws = new SockJS(localStorage.getItem('url') + "/stomp");
+            this.client = Stomp.over(ws);
+            that.client.connect({},
+                function () {
+                    that.status.setValue(true);
+                    that.client.subscribe(channel, function (message) {
+                        that.myglobal.channelWebsocket[channel].setValue(JSON.parse(message.body));
+                    });
+                },
+                function(error) {
+                    that.status.setValue(false);
+                    that.threat=setTimeout(
+                        function(){
                             that.onConnect(channel);
-                    }, 15000
-                );
-            }
-        );
+                        }, 15000
+                    );
+                }
+            );
+        }catch (e){
+            console.log('revento')
+        }
+
+    }
+    ngOnDestroy(){
+        console.log('rrere');
     }
 }
