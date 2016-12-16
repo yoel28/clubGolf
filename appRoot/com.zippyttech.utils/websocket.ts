03887618997ae561b7b0a812d4a9173888e2e8be
yoel <yo3l.m18@gmail.com>
2016-12-16 14:19:28 -0400

    nuevo controlador de websocket

--- appRoot/com.zippyttech.utils/websocket.ts
+++ appRoot/com.zippyttech.utils/websocket.ts
@@ -1,5 +1,4 @@
-import {Injectable, OnDestroy} from '@angular/core';
-import {globalService} from "./globalService";
+import {Injectable} from '@angular/core';
 import {FormControl} from "@angular/forms";
 
 
@@ -7,57 +6,95 @@ declare var SockJS:any;
 declare var Stomp:any;
 
 @Injectable()
-export class WebSocket implements OnDestroy{
-    /*
-    * this.ws.client.ws.readyState
-    * 0= Nada
-    * 1= Conectado
-    * 3= Desconectado
-    * */
+export class WebSocket {
+    /**
+     * this.ws.client.ws.readyState
+     * 0= Nada
+     * 1= Conectado
+     * 3= Desconectado
+     * */
 
-    public client:any;
-    public status:FormControl;
-    public threat:any;
 
-    constructor(public myglobal:globalService){
-        this.status = new FormControl(false);
+    public webSocket={};
+
+    constructor(){}
+
+    setWebSocket(channel){
+        if(!this.webSocket[channel]){
+            this.webSocket[channel]={
+                'client':null,
+                'data': new FormControl(),
+                'status':new FormControl('close'),
+                'reconnect':0,
+                'forceClose':false,
+                'subscribers':{
+                    'client':null,
+                }
+            }
+        }
+        else{
+            this.webSocket[channel].reconnect=0;
+            this.webSocket[channel].forceClose=false;
+        }
     }
 
     onSocket(channel) {
-        if (!this.myglobal.channelWebsocket[channel])
-            this.myglobal.channelWebsocket[channel] = new FormControl(null);
+        this.setWebSocket(channel);
         this.onConnect(channel);
     }
-    onMessage(value){
-        this.client.send("/app/message", {priority: 9}, value);
+    onMessage(channel,value){
+        this.webSocket[channel].client.send("/app/message", {priority: 9}, value);
     }
     onConnect(channel){
         try {
-            let that = this;
-            let ws = new SockJS(localStorage.getItem('url') + "/stomp");
-            this.client = Stomp.over(ws);
-            that.client.connect({},
-                function () {
-                    that.status.setValue(true);
-                    that.client.subscribe(channel, function (message) {
-                        that.myglobal.channelWebsocket[channel].setValue(JSON.parse(message.body));
-                    });
-                },
-                function(error) {
-                    that.status.setValue(false);
-                    that.threat=setTimeout(
-                        function(){
-                            that.onConnect(channel);
-                        }, 15000
-                    );
-                }
-            );
+            if(this.checkContinue(channel)){
+
+                let that = this;
+                let ws = new SockJS(localStorage.getItem('url') + "/stomp");
+
+                that.webSocket[channel].status.setValue('init');
+                that.webSocket[channel].client = Stomp.over(ws);
+
+                that.webSocket[channel].client.connect({},
+                    function () {
+                        that.webSocket[channel].status.setValue('connect');
+                        if(that.checkContinue(channel)){
+                            that.webSocket[channel].subscribers.client = that.webSocket[channel].client.subscribe(channel, function (message) {
+                                that.webSocket[channel].data.setValue(JSON.parse(message.body));
+                            });
+                        }
+                    },
+                    function(error) {
+                        that.webSocket[channel].status.setValue('error');
+                        if(that.webSocket[channel].reconnect==0){
+                            that.webSocket[channel].reconnect++;
+                            setTimeout(function() {that.onConnect(channel)}, 15000);
+                        }
+                    }
+                );
+            }
         }catch (e){
             console.log('revento')
         }
 
     }
-    ngOnDestroy(){
-        console.log('rrere');
+    checkContinue(channel){
+        if(!this.webSocket[channel].forceClose)
+            return true;
+        this.closeWebsocket(channel);
+        return false;
+    }
+    closeWebsocket(channel){
+        this.webSocket[channel].forceClose=true;
+
+        if(this.webSocket[channel].subscribers.client)
+            this.webSocket[channel].subscribers.client.unsubscribe();
+        if(this.webSocket[channel].client){
+            if(this.webSocket[channel].client.connected)
+                this.webSocket[channel].client.disconnect();
+            this.webSocket[channel].client=null
+        }
+        this.webSocket[channel].status.setValue('close');
+        this.webSocket[channel].subscribers.client=null;
     }
 }
