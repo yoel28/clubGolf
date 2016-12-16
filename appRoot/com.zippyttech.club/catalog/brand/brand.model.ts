b6a2e564f6cfc50350dd003c9cd42b265253d133
yoel <yo3l.m18@gmail.com>
2016-12-16 17:22:28 -0400

    componente y modelo de model inicial

---
+++ appRoot/com.zippyttech.club/catalog/brand/brand.model.ts
@@ -0,0 +1,48 @@
+import {ModelBase} from "../../../com.zippyttech.common/modelBase";
+import {globalService} from "../../../com.zippyttech.utils/globalService";
+
+
+export class BrandModel extends ModelBase{
+
+    constructor(public myglobal:globalService){
+        super('BRAND','/brand/',myglobal);
+        this.initModel();
+    }
+    modelExternal() {}
+    initRules() {
+        this.rules['title'] = {
+            'type': 'text',
+            'icon': 'fa fa-font',
+            'required': true,
+            'maxLength': '35',
+            'update': this.permissions.update,
+            'search': this.permissions.filter,
+            'visible': this.permissions.visible,
+            'key': 'title',
+            'title': 'Título',
+            'placeholder': 'Título',
+        };
+        this.rules = Object.assign({},this.rules,this.getRulesDefault())
+    }
+    initPermissions() {}
+    initParamsSearch() {
+        this.paramsSearch.title="Buscar marca";
+        this.paramsSearch.placeholder="Ingrese la marca";
+    }
+    initParamsSave() {
+        this.paramsSave.title="Agregar marca"
+    }
+    initRuleObject() {
+        this.ruleObject.title="Marca";
+        this.ruleObject.placeholder="Ingrese marca";
+        this.ruleObject.key="brand";
+        this.ruleObject.code="brandId";
+        this.ruleObject.keyDisplay = "brandCode";
+
+    }
+    initRulesSave() {
+        this.rulesSave = Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+    }
+
+}
