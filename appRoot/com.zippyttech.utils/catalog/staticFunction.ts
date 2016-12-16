9d020eb7b3b610917c52cab8bf764ae6365249c0
yoel <yo3l.m18@gmail.com>
2016-12-16 17:41:15 -0400

    espacio entre clases

--- appRoot/com.zippyttech.utils/catalog/staticFunction.ts
+++ appRoot/com.zippyttech.utils/catalog/staticFunction.ts
@@ -69,7 +69,7 @@ export class StaticFunction {
     }
 
     public static classCol(lg=12,md=12,sm=12,xs=12){
-        return ' col-lg-'+lg+' col-md-'+md+'	col-sm-'+sm+' col-xs-'+xs;
+        return ' col-lg-'+lg+' col-md-'+md+' col-sm-'+sm+' col-xs-'+xs;
     }
     public static classOffset(lg=0,md=0,sm=0,xs=0){
         return ' col-lg-offset-'+lg+' col-md-offset-'+md+' col-sm-offset-'+sm+' col-xs-offset-'+xs;
