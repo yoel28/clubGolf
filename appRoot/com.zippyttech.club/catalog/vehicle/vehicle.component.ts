58d82629d7c1ac96537752a41b4ce93a1e90d4bb
yoel <yo3l.m18@gmail.com>
2016-12-19 12:27:08 -0400

    tag

--- appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
+++ appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {VehicleModel} from "./vehicle.model";
 import {globalService} from "../../../com.zippyttech.utils/globalService";
 import {BaseViewInstance} from "../../../com.zippyttech.ui/view/base/baseView.instance";
+import {TagModel} from "../tag/tag.model";
 
 declare var SystemJS:any;
 @Component({
@@ -11,12 +12,18 @@ declare var SystemJS:any;
 })
 export class VehicleComponent extends BaseViewInstance{
 
+    public tag:any;
     constructor(public myglobal:globalService) {
         super();
     }
 
     initModel() {
         this.model= new VehicleModel(this.myglobal);
+        this.tag= new TagModel(this.myglobal);
+
+        this.model.rules['tags']=this.tag.ruleObject;
+        this.model.rules['tags'].multiple=true;
+        this.model.rules['tags'].paramsSearch.where="&where="+encodeURI("[['op':'isNull','field':'vehicle.id']]");
     }
 
     initViewOptions() {
