fcdc3063a6166d5b586bf8f5c76ffb9dd5ba2e6a
Luis González <gonzalezl@zippyttech.com>
2016-12-21 12:53:17 +0000

    el opcion del where es 'or' en minuscula no 'OR'

--- appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
+++ appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
@@ -23,7 +23,7 @@ export class VehicleComponent extends BaseViewInstance{
 
         this.model.rules['tags']=this.tag.ruleObject;
         this.model.rules['tags'].multiple=true;
-        this.model.rules['tags'].paramsSearch.where="&where="+encodeURI("[['OR':[['op':'isNull','field':'vehicle.id'],['op':'eq','field':'vehicle.id','value':data.id]]]]");
+        this.model.rules['tags'].paramsSearch.where="&where="+encodeURI("[['or':[['op':'isNull','field':'vehicle.id'],['op':'eq','field':'vehicle.id','value':data.id]]]]");
         this.model.rules['tags'].paramsSearch.eval='this.model.rules[key].paramsSearch.where = this.model.rules[key].paramsSearch.where.replace("data.id",data.id)';
 
         //this.model.rules['tags'].required = true;
