f14439bc8b892a4e4933105ec6ec68ab749ba4cf
yo3l28 <yo3l.m18@gmail.com>
2016-12-21 08:18:36 -0400

    parametros opcionales

--- appRoot/com.zippyttech.rest/http-utils.ts
+++ appRoot/com.zippyttech.rest/http-utils.ts
@@ -4,43 +4,45 @@ import {ToastyConfig, ToastyService, ToastData, ToastOptions} from "ng2-toasty";
 
 export class HttpUtils {
 
-    constructor(public http:Http,public toastyService:ToastyService,public toastyConfig:ToastyConfig) {
+    constructor(public http:Http,public toastyService?:ToastyService,public toastyConfig?:ToastyConfig) {
     }
 
     addToast(title,message,type='info',time=10000) {
 
-        var toastOptions:ToastOptions = {
-            title: title,
-            msg: message,
-            showClose: true,
-            timeout: time,
-            theme: 'bootstrap',
-            onAdd: (toast:ToastData) => {
-                console.log('Toast ' + toast.id + ' has been added!');
-            },
-            onRemove: function(toast:ToastData) {
-                console.log('Toast ' + toast.id + ' has been removed!');
-            }
-        };
+        if(this.toastyService) {
+            var toastOptions:ToastOptions = {
+                title: title,
+                msg: message,
+                showClose: true,
+                timeout: time,
+                theme: 'bootstrap',
+                onAdd: (toast:ToastData) => {
+                    console.log('Toast ' + toast.id + ' has been added!');
+                },
+                onRemove: function(toast:ToastData) {
+                    console.log('Toast ' + toast.id + ' has been removed!');
+                }
+            };
 
-        switch (type){
-            case 'info':
-                this.toastyConfig.position='top-right';
-                this.toastyService.info(toastOptions);
-                break;
-            case 'success':
-                this.toastyService.success(toastOptions);
-                break;
-            case 'wait':
-                this.toastyService.wait(toastOptions);
-                break;
-            case 'error':
-                this.toastyConfig.position='bottom-center';
-                this.toastyService.error(toastOptions);
-                break;
-            case 'warning':
-                this.toastyService.warning(toastOptions);
-                break;
+            switch (type){
+                case 'info':
+                    this.toastyConfig.position='top-right';
+                    this.toastyService.info(toastOptions);
+                    break;
+                case 'success':
+                    this.toastyService.success(toastOptions);
+                    break;
+                case 'wait':
+                    this.toastyService.wait(toastOptions);
+                    break;
+                case 'error':
+                    this.toastyConfig.position='bottom-center';
+                    this.toastyService.error(toastOptions);
+                    break;
+                case 'warning':
+                    this.toastyService.warning(toastOptions);
+                    break;
+            }
         }
     }
 
