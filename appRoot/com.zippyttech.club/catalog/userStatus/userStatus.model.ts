5858bde284f4a97525f27479ec05070a1a04cf1c
yoel <yo3l.m18@gmail.com>
2016-12-22 13:25:33 -0400

    add status user required

--- appRoot/com.zippyttech.club/catalog/userStatus/userStatus.model.ts
+++ appRoot/com.zippyttech.club/catalog/userStatus/userStatus.model.ts
@@ -4,19 +4,30 @@ import {globalService} from "../../../com.zippyttech.utils/globalService";
 export class UserStatusModel extends ModelBase{
 
     constructor(public myglobal:globalService){
-        super('US_STATUS','/status/users/',myglobal);
+        super('US_STATUS','/statuses/',myglobal);
         this.initModel();
     }
     modelExternal() {}
     initRules(){
         this.rules['title']={
             'type': 'text',
+            'required':true,
             'update':this.permissions.update,
             'search':this.permissions.filter,
             'visible':this.permissions.visible,
             'key': 'title',
             'title': 'Título',
             'placeholder': 'Título',
+        };
+        this.rules['code']={
+            'type': 'text',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key': 'code',
+            'title': 'Código',
+            'placeholder': 'Código',
         };
         this.rules = Object.assign({},this.rules,this.getRulesDefault());
     }
