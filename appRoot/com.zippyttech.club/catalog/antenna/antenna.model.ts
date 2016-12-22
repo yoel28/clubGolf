e51486bc0e5e37ca08f8daeb468b5f0189c24402
yoel <yo3l.m18@gmail.com>
2016-12-22 14:25:59 -0400

    antenna

---
+++ appRoot/com.zippyttech.club/catalog/antenna/antenna.model.ts
@@ -0,0 +1,56 @@
+import {ModelBase} from "../../../com.zippyttech.common/modelBase";
+import {globalService} from "../../../com.zippyttech.utils/globalService";
+
+export class AntennaModel extends ModelBase{
+
+    constructor(public myglobal:globalService){
+        super('ANT','/antennas/',myglobal);
+        this.initModel();
+    }
+    modelExternal() {}
+    initRules(){
+        this.rules['title']={
+            'type': 'text',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key': 'title',
+            'title': 'Título',
+            'placeholder': 'Título',
+        };
+        this.rules['code']={
+            'type': 'text',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key': 'code',
+            'title': 'Código',
+            'placeholder': 'Código',
+        };
+        this.rules = Object.assign({},this.rules,this.getRulesDefault());
+    }
+    initPermissions() {}
+    initParamsSearch() {
+        this.paramsSearch.title="Buscar antena";
+        this.paramsSearch.placeholder="Ingrese la antena";
+        this.paramsSearch.label.title="Titulo: ";
+        this.paramsSearch.label.detail="Código: "
+    }
+    initParamsSave() {
+        this.paramsSave.title="Agregar antena"
+    }
+    initRuleObject() {
+        this.ruleObject.title="Antenas";
+        this.ruleObject.placeholder="Ingrese la antena";
+        this.ruleObject.key="antenna";
+        this.ruleObject.keyDisplay="antennaTitle";
+        this.ruleObject.code="antennaId";
+    }
+    initRulesSave() {
+        this.rulesSave = Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+    }
+
+}
