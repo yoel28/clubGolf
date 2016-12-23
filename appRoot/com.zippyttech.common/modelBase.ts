d3ab9fadc10eae9fccb9eec39367f8cff7461f6a
yoel <yo3l.m18@gmail.com>
2016-12-23 11:34:23 -0400

    objectOrSave - > objeto para crear o enlazar

--- appRoot/com.zippyttech.common/modelBase.ts
+++ appRoot/com.zippyttech.common/modelBase.ts
@@ -140,7 +140,7 @@ export abstract class ModelBase{
             "keyDisplay": "keyDefault",
             "title": "TipoDefault",
             'object': true,
-            'objectOrSave': false,
+            'objectOrSave': null,
             'code': 'default',
             'prefix':'',
             "placeholder": "PlaceHolder default",
