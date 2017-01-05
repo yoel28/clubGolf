57c2d9297d807c843672da4465d06ba57f6aeee9
yoel <yo3l.m18@gmail.com>
2017-01-05 10:24:05 -0400

    variables publicas

--- appRoot/com.zippyttech.common/DependenciesBase.ts
+++ appRoot/com.zippyttech.common/DependenciesBase.ts
@@ -3,9 +3,14 @@ import {Http} from "@angular/http";
 import {globalService} from "../com.zippyttech.utils/globalService";
 import {ToastyService, ToastyConfig} from "ng2-toasty";
 import {Injectable} from "@angular/core";
+import {StaticValues} from "../com.zippyttech.utils/catalog/staticValues";
+import {StaticFunction} from "../com.zippyttech.utils/catalog/staticFunction";
 
 @Injectable()
 export class DependenciesBase {
+    public msg = StaticValues.msg;
+    public classCol=StaticFunction.classCol;
+    public classOffset=StaticFunction.classOffset;
 
     constructor(
                 public router: Router,
