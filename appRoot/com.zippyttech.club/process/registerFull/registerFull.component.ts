e13cd48b4b1806701549c946fb5bfdfde4a655c8
yoel <yo3l.m18@gmail.com>
2017-01-09 16:01:55 -0400

    cargar user en el form si userId existe.. se enviar ese....

--- appRoot/com.zippyttech.club/process/registerFull/registerFull.component.ts
+++ appRoot/com.zippyttech.club/process/registerFull/registerFull.component.ts
@@ -9,6 +9,7 @@ import {Http} from "@angular/http";
 import {ToastyService, ToastyConfig} from "ng2-toasty";
 import {FormGroup, Validators, FormControl} from "@angular/forms";
 import {AnimationsManager} from "../../../com.zippyttech.ui/animations/AnimationsManager";
+import {UserModel} from "../../../com.zippyttech.access/user/user.model";
 
 declare var SystemJS:any;
 @Component({
@@ -27,6 +28,8 @@ export class RegisterFullComponent extends RestController implements OnInit{
     public classOffset = StaticFunction.classOffset;
 
     public user:any;
+    public userObject:any={};
+    public userObjectInstance:any;
     public vehicle:any=[];
 
     constructor(public http:Http,public toastyService:ToastyService,public toastyConfig:ToastyConfig,public myglobal:globalService) {
@@ -47,6 +50,8 @@ export class RegisterFullComponent extends RestController implements OnInit{
 
     initModel() {
         this.model= new RegisterFullModel(this.myglobal);
+        this.userObject['id']= (new UserModel(this.myglobal)).ruleObject;
+        this.userObject['id'].key='id';
     }
 
     initViewOptions() {
@@ -69,7 +74,11 @@ export class RegisterFullComponent extends RestController implements OnInit{
         if(event)
             event.preventDefault();
         let that = this;
-        let data = Object.assign({},this.user.getFormValues());
+        let data;
+        if(this.userObjectInstance.form.valid)
+            data = Object.assign({},this.userObjectInstance.getFormValues());
+        else
+            data = Object.assign({},this.user.getFormValues());
         data['vehicles']=[];
         this.instanceVehicle.forEach(obj=>{
             if(obj && obj.form){
@@ -89,16 +98,16 @@ export class RegisterFullComponent extends RestController implements OnInit{
         this.dataOk = true;
     }
     isValidForm():boolean{
-        let valid=true;
-        if(this.user && this.user.form)
-            valid = this.user.form.valid;
+        let count=0;
+        if( !(this.user && this.user.form && this.user.form.valid) && !(this.userObjectInstance && this.userObjectInstance.form && this.userObjectInstance.form.valid))
+            return false;
 
         this.instanceVehicle.forEach(obj=>{
             if(obj && obj.form && !obj.form.valid){
-                valid=false;
+                count ++;
                 return;
             }
         });
-        return valid;
+        return count==0?true:false;
     }
 }
