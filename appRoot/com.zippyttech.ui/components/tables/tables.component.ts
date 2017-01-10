bd3bbd05d7d0d4bc0b7b1e1e305a6f807f7b3fb8
Karolin Porras <porrask@zippyttech.com>
2017-01-10 15:49:12 -0400

    Condicion de params && params.actions in table

--- appRoot/com.zippyttech.ui/components/tables/tables.component.ts
+++ appRoot/com.zippyttech.ui/components/tables/tables.component.ts
@@ -43,9 +43,11 @@ export class TablesComponent extends RestController implements OnInit {
 
     ngOnInit()
     {
-        this.keyActions=Object.keys(this.params.actions);
+        this.keyActions=[];
+        if(this.params && this.params.actions)
+            this.keyActions=Object.keys(this.params.actions);
         this.getInstance = new EventEmitter();
-        this.setEndpoint(this.params.endpoint);
+        this.setEndpoint(this.params? this.params.endpoint:'');
         this.getListObjectNotReferenceSave();
     }
     ngAfterViewInit() {
