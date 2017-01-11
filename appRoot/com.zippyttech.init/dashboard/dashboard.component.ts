267c7abdc7e043d01617255c23a0d53363c8c5a3
Alex <riveram@zippyttech.com>
2017-01-11 09:52:55 -0400

    dashboard

--- appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
+++ appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 import {globalService} from "../../com.zippyttech.utils/globalService";
+import {ControllerBase} from "../../com.zippyttech.common/ControllerBase";
+import {DependenciesBase} from "../../com.zippyttech.common/DependenciesBase";
+import {RecordModel} from "../../com.zippyttech.club/catalog/record/record.model";
+import {TradeModel} from "../../com.zippyttech.club/catalog/trade/trade.model";
 
 declare var SystemJS:any;
 
@@ -9,12 +13,23 @@ declare var SystemJS:any;
     templateUrl: SystemJS.map.app+'com.zippyttech.init/dashboard/index.html',
     styleUrls: [ SystemJS.map.app+'com.zippyttech.init/dashboard/style.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent extends ControllerBase implements OnInit{
+    private record:RecordModel;
+    private trade:TradeModel;
 
+    constructor(public myglobal:globalService,public http:Http,public db:DependenciesBase) {
+        super(db,'NOPREFIX','/dashboard/');
+        this.initModel();
+    }
 
-    constructor(public myglobal:globalService,public http:Http) {}
+    ngOnInit():void {
 
-    ngOnInit():void{}
+    }
+
+    initModel(){
+        let visibles:String[] = ["dateIn","userName","vehicle","userType"];
+
+    }
 
 }
 
