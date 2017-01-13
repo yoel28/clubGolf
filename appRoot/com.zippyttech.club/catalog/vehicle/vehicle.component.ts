de201a50b42958434947debea8b40729bb286ab8
yoel <yo3l.m18@gmail.com>
2017-01-13 16:43:28 -0400

    eliminado nombre de fileld

--- appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
+++ appRoot/com.zippyttech.club/catalog/vehicle/vehicle.component.ts
@@ -28,7 +28,6 @@ export class VehicleComponent extends BaseViewInstance{
         this.model.rules['tags'].multiple=true;
         this.model.rules['tags'].paramsSearch.where="&where="+encodeURI("[['or':[['op':'isNull','field':'vehicle.id'],['op':'eq','field':'vehicle.id','value':data.id]]]]");
         this.model.rules['tags'].paramsSearch.eval='this.model.rules[key].paramsSearch.where = this.model.rules[key].paramsSearch.where.replace("data.id",data.id)';
-        this.model.rules['tags'].paramsSearch.field = 'tags';
 
         //this.model.rules['tags'].required = true;
         this.model.rules['tags'].reference = true;
