aca0e7ee9b939a21bcb3ea2456eefa56ea685dcf
yoel <yo3l.m18@gmail.com>
2017-01-16 17:02:11 -0400

    agregados tags en recrod

--- appRoot/com.zippyttech.club/catalog/record/record.component.ts
+++ appRoot/com.zippyttech.club/catalog/record/record.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {BaseViewInstance} from "../../../com.zippyttech.ui/view/base/baseView.instance";
 import {RecordModel} from "./record.model";
 import {DependenciesBase} from "../../../com.zippyttech.common/DependenciesBase";
+import {TagModel} from "../tag/tag.model";
 
 declare var SystemJS:any;
 @Component({
@@ -17,6 +18,11 @@ export class RecordComponent extends BaseViewInstance{
 
     initModel() {
         this.model= new RecordModel(this.db);
+
+        this.model.rules['tags']=(new TagModel(this.db)).ruleObject;
+        this.model.rules['tags'].search=false;
+        this.model.rules['tags'].multiple=true;
+        this.model.rules['tags'].permissions={};
 
         this.model.rules['userContractCode']={
             'type': 'text',
