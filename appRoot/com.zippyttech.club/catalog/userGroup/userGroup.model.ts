359d5ae9c4585b4d9bfa4638b8b529d9e7a40e15
yoel <yo3l.m18@gmail.com>
2017-01-20 17:57:08 -0400

    use debug

---
+++ appRoot/com.zippyttech.club/catalog/userGroup/userGroup.model.ts
@@ -0,0 +1,62 @@
+import {ModelBase} from "../../../com.zippyttech.common/modelBase";
+import {DependenciesBase} from "../../../com.zippyttech.common/DependenciesBase";
+
+export class UserGroupModel extends ModelBase{
+
+    constructor(public db:DependenciesBase){
+        super(db,'US_GROUP','/groups/');
+        this.initModel();
+    }
+    modelExternal() {}
+    initRules() {
+        this.rules['title'] = {
+            'type': 'text',
+            'icon': 'fa fa-font',
+            'required': true,
+            'maxLength': '35',
+            'update': this.permissions.update,
+            'search': this.permissions.filter,
+            'visible': this.permissions.visible,
+            'key': 'title',
+            'title': 'Título',
+            'placeholder': 'Título',
+        };
+        this.rules['code'] = {
+            'type': 'text',
+            'icon': 'fa fa-font',
+            'required': true,
+            'maxLength': '35',
+            'update': this.permissions.update,
+            'search': this.permissions.filter,
+            'visible': this.permissions.visible,
+            'key': 'code',
+            'title': 'Código',
+            'placeholder': 'Código',
+        };
+        this.globalOptional();
+        this.rules = Object.assign({},this.rules,this.getRulesDefault());
+    }
+    initPermissions() {}
+    initParamsSearch() {
+        this.paramsSearch.title="Buscar grupo de usuario";
+        this.paramsSearch.placeholder="Ingrese el grupo de usuario";
+        this.paramsSearch.label.title="Código: ";
+        this.paramsSearch.label.detail="Nombre: "
+    }
+    initParamsSave() {
+        this.paramsSave.title="Agregar grupo de usuario"
+    }
+    initRuleObject() {
+        this.ruleObject.title="Grupo";
+        this.ruleObject.placeholder="Ingrese el grupo de usuario";
+        this.ruleObject.key="userGroup";
+        this.ruleObject.code="userGroupId";
+        this.ruleObject.keyDisplay = "userGroupTitle";
+
+    }
+    initRulesSave() {
+        this.rulesSave = Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+    }
+
+}
