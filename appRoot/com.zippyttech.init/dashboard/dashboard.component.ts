d35cd0c2bea5872f7c85f687a7d5d3745bebfd63
Alex <riveram@zippyttech.com>
2017-01-20 19:27:22 -0400

    Update: adding chart-vew in dashboard

--- appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
+++ appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
@@ -7,13 +7,9 @@ import {IListActionData, ListActionComponent} from "../../com.zippyttech.ui/comp
 import {GetbackModel} from "../../com.zippyttech.club/process/getBack/getback.model";
 import {DashboardModel} from "./dashboard.model";
 import {FormControl} from "@angular/forms";
+import {IChartData} from "../../com.zippyttech.ui/components/chartview/chartview.component";
 
-/*
-const Highcharts = require('highcharts');
-const Highcharts3d = require('highcharts/highcharts-3d.src');
-Highcharts.setOptions({
-    colors: ['#058DC7', '#50B432', '#ED561B']
-});*/
+
 
 declare var SystemJS:any;
 declare var jQuery:any;
@@ -31,23 +27,39 @@ export class DashboardComponent extends ControllerBase implements OnInit, DoChec
     public guestRemove:FormControl = new FormControl();
     public qrString:string = '';
     public qrHidden: boolean;
-
-    public chart1options:Highcharts.Options;
+    public chvwEntries:IChartData;
+    public chvwProducts:IChartData;
 
     constructor(public myglobal:globalService,public http:Http,public db:DependenciesBase) {
         super(db,'DASH','/dashboard/');
-        this.initCharts();
     }
 
     ngOnInit():void {
         super.ngOnInit();
         this.initActions();
+        this.initChartView();
     }
 
     initModel(){
         this.model = new DashboardModel(this.db);
     }
 
+    ngDoCheck() {
+        if(jQuery('#reader').hasClass('reader-hide'))
+            this.qrHidden = true;
+
+        if(!(this.model.qr && this.model.qr.dataList && this.model.qr.dataList.id)) {
+            jQuery('#reader').find('.box-body,.box-footer').collapse('hide');
+            jQuery('#reader').find('.box').addClass('collapsed-box');
+        }
+        else{
+            jQuery('#reader').find('.box').removeClass('collapsed-box');
+            jQuery('#reader').find('.box-body,.box-footer').collapse('show');
+        }
+    }
+
+
+    //LIST-ACTION GROUP
     private initActions() {
         //Productos
         let modelAction = new GetbackModel(this.db);
@@ -113,6 +125,65 @@ export class DashboardComponent extends ControllerBase implements OnInit, DoChec
         }
     }
 
+
+
+    //CHART-VIEW
+    public initChartView(){
+
+        this.chvwEntries ={
+            title: "Entradas",
+            endpoint: "/reports/entries/",
+            options:{
+                chart: {
+                    type: 'areaspline'
+                },
+                xAxis: {
+                    tickmarkPlacement: 'on'
+                },
+                yAxis: {
+                    title: { text: 'N° de entradas' },
+                },
+                tooltip: {
+                    valueSuffix: ' entradas',
+                    split:true,
+                    crosshairs: true
+                },
+                plotOptions: {
+                    areaspline:
+                    {
+                        fillOpacity: 0.5
+                    }
+                }
+            }
+        }
+
+        this.chvwProducts ={
+            title: "Productos",
+            endpoint: "/reports/products/",
+            options:{
+                chart: {
+                    type: 'column'
+                },
+                xAxis: {
+                    tickmarkPlacement: 'on'
+                },
+                yAxis: {
+                    title: { text: 'N° de entradas' },
+                },
+                tooltip: {
+                    valueSuffix: ' entradas',
+                    split:true,
+                    crosshairs: true
+                }
+            }
+        }
+    }
+
+
+
+
+
+    //QR READER
     searchQr(event){
         if(event)
             event.preventDefault();
@@ -136,10 +207,7 @@ export class DashboardComponent extends ControllerBase implements OnInit, DoChec
         }
     }
 
-
-
-    public loadAttendings(event)
-    {
+    public loadAttendings(event){
         let that = this;
         let callback = (response)=>{
             that.guestRemove.setValue(this.model.qr.dataList.id);
@@ -152,27 +220,10 @@ export class DashboardComponent extends ControllerBase implements OnInit, DoChec
         this.model.qr.dataList = {};
     }
 
-    private observableAction(context:ListActionComponent)
-    {
+    private observableAction(context:ListActionComponent){
 
         if(context.data.observable.watch.value)
             context.data.model.spliceId(context.data.observable.watch.value);
-    }
-
-
-
-    ngDoCheck() {
-        if(jQuery('#reader').hasClass('reader-hide'))
-            this.qrHidden = true;
-
-        if(!(this.model.qr && this.model.qr.dataList && this.model.qr.dataList.id)) {
-            jQuery('#reader').find('.box-body,.box-footer').collapse('hide');
-            jQuery('#reader').find('.box').addClass('collapsed-box');
-        }
-        else{
-            jQuery('#reader').find('.box').removeClass('collapsed-box');
-            jQuery('#reader').find('.box-body,.box-footer').collapse('show');
-        }
     }
 
     private readerClick(){
@@ -187,49 +238,6 @@ export class DashboardComponent extends ControllerBase implements OnInit, DoChec
         }
     }
 
-    public initCharts()
-    {
-        let that = this;
-        let callback = (response)=>{
-            that.loadChart(response.json());
-        };
-        this.httputils.doGet('/reports/entries/2017/',callback,null,false);
-    }
-
-    public loadChart(JSONrespose)
-    {
-
-        let data:any = {};
-
-        Object.assign(data, JSONrespose);
-
-        this.chart1options = {
-            chart: {
-                type: 'areaspline'
-            },
-            title: {
-                text: 'ENTRADAS'
-            },
-            xAxis: {
-                categories: data.categories,
-                tickmarkPlacement: 'on'
-            },
-            yAxis: {
-                title: { text: 'N° de entradas' },
-            },
-            tooltip: {
-                valueSuffix: ' entradas'
-            },
-            plotOptions: {
-                series: {
-                    shadow: true
-                },
-                line:{}
-            },
-            series: data.list,
-            credits:false
-        };
-    }
 }
 
 
