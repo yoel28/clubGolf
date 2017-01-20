8cf307abac5a4942d8966daf5232efa324c59b17
Alex <riveram@zippyttech.com>
2017-01-20 19:26:27 -0400

    Update: chart-vew

--- appRoot/com.zippyttech.ui/components/chartview/chartview.component.ts
+++ appRoot/com.zippyttech.ui/components/chartview/chartview.component.ts
@@ -1,58 +1,110 @@
 import {Component, NgModule} from "@angular/core";
-import {SearchComponent} from "../search/search.component";
-import {SaveComponent} from "../save/save.component";
-import {XEditable} from "../../../com.zippyttech.utils/directive/xEditable";
-import {ColorPicker} from "../../../com.zippyttech.utils/directive/colorPicker";
 import {ControllerBase} from "../../../com.zippyttech.common/ControllerBase";
 import {DependenciesBase} from "../../../com.zippyttech.common/DependenciesBase";
 
+const Highcharts = require('highcharts');/*
+ const Highcharts3d = require('highcharts/highcharts-3d.src');
+ Highcharts.setOptions({
+ colors: ['#058DC7', '#50B432', '#ED561B']
+ });*/
+
 declare var SystemJS:any;
 declare var moment:any;
+declare var jQuery:any;
 
 export interface IChartData
 {
     endpoint:string;
-    options:Highcharts.Options;
+    title:string;
+    options:{};
 }
 /*
 @NgModule({
     imports:[XEditable,ColorPicker,SearchComponent,SaveComponent]
 })*/
 @Component({
+    moduleId: module.id,
     selector: 'chart-view',
-    templateUrl: SystemJS.map.app+'/com.zippyttech.ui/components/accordion/index.html',
-    styleUrls: [ SystemJS.map.app+'/com.zippyttech.ui/components/accordion/style.css'],
+    templateUrl: 'template.html',
+    styleUrls: ['style.css'],
     inputs:['chartData']
 })
 export class ChartViewComponent extends ControllerBase
 {
 
     public chartData:IChartData;
+    public chartId:string;
+    public year:number;
+    public month:number;
+    public chartInstance:Highcharts.ChartObject;
 
     constructor(public db:DependenciesBase){
         super(db);
-        let that = this;
-        let callback = (response)=>{
-            that.loadChart(response.json());
-        };
-        this.httputils.doGet(this.chartData.endpoint+((new Date).getFullYear()),callback,null,false);
+        this.year = (new Date).getFullYear();
+        this.month = -1;
     }
-
-    public loadChart(JSONrespose)
-    {
-
-        let data:{list:[{}],categories:string[]};
-
-        Object.assign(data, JSONrespose);
-
-        this.chartData.options.xAxis["categories"] = data.categories;
-        this.chartData.options.series = data.list;
-        this.chartData.options.credits = false;
-    }
-
 
     initModel() {
+        this.chartId = "CHART_"+moment().valueOf();
+        this.chartData.options["chart"].renderTo = this.chartId;
+        this.chartData.options["credits"] = false;
+        this.chartRefresh();
     }
 
+    public onPointSelect(event)
+    {
+        if(this.month < 0) {
+            this.month = event.context.x + 1;
+            this.chartRefresh();
+        }
+    }
+
+    public change(dir:number){
+        if(this.month < 0)
+            this.year += dir;
+        else
+            this.month += dir;
+        this.chartRefresh();
+    }
+
+    public saveInstance(instance)
+    {
+        this.chartInstance = instance;
+    }
+
+    public chartRefresh(){
+        let that = this;
+        let callback = (response)=>{
+            let data:any = {};
+            let that = this;
+            Object.assign(data,response.json());
+            for(let ob of data.list) {
+                let newData = {
+                    name: ob["name"],
+                    data: ob["data"],
+                    allowPointSelect: true
+                };
+                Object.assign(ob, newData);
+            }
+            this.chartData.options["title"] = {
+                                                text: this.getTitle()
+                                            },
+            this.chartData.options["xAxis"].categories = data.categories;
+            this.chartData.options["series"] = data.list;
+            console.log(this.chartData.options);
+            console.log(''+this.year+"/"+this.month);
+            if(this.chartInstance) {
+                this.chartInstance = new Highcharts.chart(this.chartData.options);
+            }
+        };
+        this.httputils.doGet(this.chartData.endpoint+this.year+'/'+((this.month<0)?"":this.month),callback,null,false);
+    }
+
+    public getTitle():string{
+        let months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+        if(this.month < 0)
+            return ''+this.year;
+        return months[this.month-1];
+    }
 
 }
