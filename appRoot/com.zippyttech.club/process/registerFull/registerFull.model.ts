34712f27d6380e21874726c9933fcdbf3f23faae
yoel <yo3l.m18@gmail.com>
2017-03-08 12:06:28 -0400

    onlyRequired

--- appRoot/com.zippyttech.club/process/registerFull/registerFull.model.ts
+++ appRoot/com.zippyttech.club/process/registerFull/registerFull.model.ts
@@ -1,7 +1,6 @@
 import {ModelBase} from "../../../com.zippyttech.common/modelBase";
 import {UserModel} from "../../../com.zippyttech.access/user/user.model";
 import {VehicleModel} from "../../catalog/vehicle/vehicle.model";
-import {TagModel} from "../../catalog/tag/tag.model";
 import {DependenciesBase} from "../../../com.zippyttech.common/DependenciesBase";
 import {IModelActions, ModelRoot} from "../../../com.zippyttech.common/modelRoot";
 
@@ -10,7 +9,6 @@ export class RegisterFullModel extends ModelBase{
 
     public user:any;
     public vehicle:any;
-    public tags:any;
 
 
     constructor(public db:DependenciesBase){
@@ -21,26 +19,19 @@ export class RegisterFullModel extends ModelBase{
         this.user = new UserModel(this.db);
         this.user.rules = (<ModelRoot>this.user).getIncludeKeys(['email','username','name']);
         this.vehicle = new VehicleModel(this.db,false);
-        this.tags = new TagModel(this.db);
     }
     initRules() {
 
         this.rules['user'] =  this.user.ruleObject;
         this.rules['user'].update= this.permissions.update;
+        this.rules['user'].paramsSave.onlyRequired=true;
 
         this.rules['vehicle'] =  this.vehicle.ruleObject;
         this.rules['vehicle'].update= this.permissions.update;
+        this.rules['vehicle'].paramsSave.onlyRequired=true;
 
-        delete this.rules['vehicle'].rulesSave['user'];
-        delete this.rules['vehicle'].rulesSave['year'];
-        delete this.rules['vehicle'].rulesSave['color'];
-        delete this.rules['vehicle'].rulesSave['model'];
-        delete this.rules['vehicle'].rulesSave['vehicleType'];
-        delete this.rules['vehicle'].rulesSave['detail'];
+        delete this.rules['vehicle'].rulesSave.user;
 
-
-        this.rules['tags'] =  this.tags.ruleObject;
-        this.rules['tags'].update= this.permissions.update;
     }
     initPermissions() {}
     initParamsSearch() {}
