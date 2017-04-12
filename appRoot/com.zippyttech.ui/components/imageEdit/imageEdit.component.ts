4dfab8e3eb3e62b3d1fb30dc57048a26399e7590
Alex <riveram@zippyttech.com>
2017-04-12 11:14:16 -0400

    add type inline

--- appRoot/com.zippyttech.ui/components/imageEdit/imageEdit.component.ts
+++ appRoot/com.zippyttech.ui/components/imageEdit/imageEdit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, AfterContentInit} from '@angular/core';
+import {Component, EventEmitter, AfterContentInit, ViewChild, ElementRef, OnInit} from '@angular/core';
 import {StaticValues} from "../../../com.zippyttech.utils/catalog/staticValues";
 
 declare var SystemJS:any;
@@ -10,16 +10,16 @@ var jQuery = require('jquery');
     selector: 'image-edit-view',
     templateUrl: 'index.html',
     styleUrls: [ 'style.css'],
-    inputs:['params','image','default','edit'],
-    outputs:['out'],
+    inputs:['params','image','default','edit','type'],
+    outputs:['out']
 })
 export class ImageEditComponent implements AfterContentInit{
-
+    @ViewChild('name') name:ElementRef;
     public configId=moment().valueOf();
     public out:any;
     public pathElements=StaticValues.pathElements;
     public edit:boolean = true;
-
+    public type:string = 'block';
     public params:any={};
 
     public image:string="";
@@ -30,12 +30,25 @@ export class ImageEditComponent implements AfterContentInit{
     }
 
     ngAfterContentInit(){
-        jQuery(".file-input").addClass("btn-i");
+        let f1 = document.querySelector('.file-input');
+        let ifile = document.querySelector('#img-file');
+        if(f1)
+            f1.classList.add("btn-i");
     }
-    saveImage(data){
+    saveImage(event){
         this.out.emit(this.image);
     }
     changeImage(data=null){
         this.image=data;
+    }
+    changeFilePath(event){
+        let file = event.srcElement.files[0];
+        let reader:FileReader = new FileReader();
+        reader.onloadend = (e)=>{
+            this.image = reader.result;
+            this.out.emit(this.image);
+        };
+        reader.readAsDataURL(file);
+        this.name.nativeElement.textContent = file.name;
     }
 }
